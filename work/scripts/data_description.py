d59eabf751c2f6c98150d337ae6bc52be30b6b90
arsenosov <arsenosov@gmail.com>
2023-12-24 18:25:43 +0300

    Fix error

--- work/scripts/data_description.py
+++ work/scripts/data_description.py
@@ -38,8 +38,6 @@ dfStats = pd.concat([dfDescribe, dfIQR])
 
 logging.info(f"Results\n{dfStats}\n")
 
-# removing NaN values to measure corellation
-dfDataFloat = dfDataFloat.notna()
 C_P = pd.DataFrame([], index=dfDataFloat.columns, columns=dfDataFloat.columns) 
 P_P = pd.DataFrame([], index=dfDataFloat.columns, columns=dfDataFloat.columns)
 C_S = pd.DataFrame([], index=dfDataFloat.columns, columns=dfDataFloat.columns)
